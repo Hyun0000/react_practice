d0c634e964c52d6367dcda7cc342370bfe7bf663
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2022-02-20 21:17:37 +0900

    #2 state에 데이터 저장(HTML 재렌더링)
    #2 state에 데이터 저장(HTML 재렌더링)

--- App.js
+++ App.js
@@ -1,42 +1,49 @@
+import React, {useState} from 'react';
+// 'react'에 있는 내장 함수(useState)를 사용하겠다고 선언하는 것
+
 import './App.css';
 
-// 이미지 import 하는 방법
-import logo from './wendy.jfif';
-
 function App() {
-  const fontStyle = 
-  {
-    color : 'green',
-    fontSize : '30px'
-  }
+  // state 만드는 방법
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '잔망루피 짱']);
+  // ES6 destructuring 문법 사용
+  // let 글제목 = ['남자 코트 추천', '강남 우동 맛집', '잔망루피 짱'];
+  // let 글제목변경 = 글제목 state 데이터를 변경(수정)하는 함수
 
-  const imgSize = 
-  {
-    width : '300px',
-    heigth : '500px'
-  }
+  // let [글제목, 글제목변경] = useState("남자 코트 추천");
+  // useState("남자 코트 추천") : state(데이터 저장 공간)를 만드는 방법
+
+  // let posts = "일반변수제목";
   
-  var data = 'Hello World!';
-  let post = "강남 고기 맛집"
-
-  function func1() {
-    return 100;
-  }
+  let [objTest, objTestFunc] = useState({name : "Kim", age : 50});
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
-        <div>{data}</div>
       </div>
 
-      <h4>{post} TOP {func1()}</h4>
-      <h4>{func1()}</h4>
+      <div className='list'>
+        <h3>{글제목[0]}</h3>
+        <h3>{글제목}</h3> {/* array를 단순 글자를 바꿔서 출력 */}
+        <h3>{objTest.name}</h3>
+        <h3>{objTest.age}</h3>
+        <p>2월 20일 발행</p>
+        <hr/>
+      </div>
 
-      <img src={logo} alt="123" style={imgSize}></img>
+      <div className='list'>
+        <h3>{글제목[1]}</h3>
+        <p>2월 21일 발행</p>
+        <hr/>
+      </div>
 
-      <div style={{color : 'blue', fontSize : '30px'}}> 글씨 </div>
-      <div style={fontStyle}> 글씨 </div>
+      <div className='list'>
+        <h3>{글제목[2]}</h3>
+        <p>2월 22일 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
